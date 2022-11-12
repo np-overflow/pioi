02928debac477290d74a60f3817f691424e15175
jimmy <s10219505@connect.np.edu.sg>
2022-11-13 00:40:02 +0800

    feat: add GlowWrapper component to handle glow logic

---
+++ components/GlowWrapper.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const target = ref(null)
+
+useEventListener(target, 'mousemove', (e) => {
+	for (const card of target.value.getElementsByClassName('glow')) {
+		const rect = card.getBoundingClientRect()
+		const x = e.clientX - rect.left
+		const y = e.clientY - rect.top
+
+		card.style.setProperty('--x', `${x}px`)
+		card.style.setProperty('--y', `${y}px`)
+	}
+})
+</script>
+
+<template>
+	<div ref="target">
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+
+</style>
