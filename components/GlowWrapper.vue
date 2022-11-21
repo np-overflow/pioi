cc478415ed1bc1161ce75e9edba389b717eceacc
notjim <90300748+jimmy-lew@users.noreply.github.com>
2022-11-21 23:13:39 +0800

    feat: add mouse leave condition for GlowWrapper

--- components/GlowWrapper.vue
+++ components/GlowWrapper.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 const target = ref(null)
 
-useEventListener(target, 'mousemove', (e) => {
-	for (const card of target.value.getElementsByClassName('glow')) {
+useEventListener(target, 'mousemove', (e: MouseEvent) => {
+	const parent = target.value
+
+	if (parent == null)
+		return
+
+	for (const card of parent.getElementsByClassName('glow') as HTMLCollectionOf<HTMLElement>) {
 		const rect = card.getBoundingClientRect()
 		const x = e.clientX - rect.left
 		const y = e.clientY - rect.top
+
+		card.style.setProperty('--x', `${x}px`)
+		card.style.setProperty('--y', `${y}px`)
+	}
+})
+
+useEventListener(target, 'mouseleave', (e: MouseEvent) => {
+	const parent = target.value
+
+	if (parent == null)
+		return
+
+	for (const card of parent.getElementsByClassName('glow') as HTMLCollectionOf<HTMLElement>) {
+		const rect = card.getBoundingClientRect()
+		const x = NaN
+		const y = NaN
 
 		card.style.setProperty('--x', `${x}px`)
 		card.style.setProperty('--y', `${y}px`)
