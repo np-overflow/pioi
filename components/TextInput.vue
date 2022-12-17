3c0dc13a4fd623e735a9931bb265a99ca2fa1429
jimmy <s10219505@connect.np.edu.sg>
2022-12-18 02:10:39 +0800

    feat: add TextInput component

---
+++ components/TextInput.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+	placeholder?: string
+	error?: string
+}>()
+
+const emits = defineEmits(['inputChange'])
+
+const input = ref('')
+</script>
+
+<template>
+	<div class="relative flex flex-col w-full">
+		<input
+			v-model="input"
+			class="w-full bg-transparent hover:bg-[#141418] rounded focus:outline-0 p-1"
+			type="text"
+			:placeholder="placeholder ? placeholder : 'Empty...'"
+			:class="error ? 'error' : ''"
+			@change="() => emits('inputChange', input)"
+		>
+		<span class="absolute top-1/2 right-0 -translate-y-1/2 text-xs px-1 text-[#fa5152]">
+			{{ error }}
+		</span>
+	</div>
+</template>
+
+<style scoped>
+.error {
+    @apply outline-[0.5px] outline outline-[#fa5152] text-[#fa5152];
+}
+</style>
