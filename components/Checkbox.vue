ea44241946e78f34f578edb7bc3245abcb000e0c
jimmy <s10219505@connect.np.edu.sg>
2022-12-17 21:26:34 +0800

    feat: add Checkbox component

---
+++ components/Checkbox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const isChecked = ref(false)
+
+const toggleChecked = () => {
+	isChecked.value = !isChecked.value
+}
+</script>
+
+<template>
+	<Icon
+		:name="isChecked ? 'material-symbols:check-box' : 'material-symbols:check-box-outline-blank'"
+		@click="toggleChecked"
+	/>
+</template>
+
+<style scoped>
+
+</style>
