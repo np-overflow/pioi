56230f8a8f8a443c28595282538e39aff9727c9a
jimmy <s10219505@connect.np.edu.sg>
2022-12-17 15:50:13 +0800

    refactor: add lyra schema type

--- types/index.ts
+++ types/index.ts
@@ -2,4 +2,10 @@ export { }
 
 declare global {
 	type Nullable<T> = T | null
+
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	type LyraSchema = {
+		_id: 'number'
+		name: 'string'
+	}
 }
