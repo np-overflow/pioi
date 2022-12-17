9cce5c11b3f80a843feae75cbd31551c206ba8dd
jimmy <s10219505@connect.np.edu.sg>
2022-12-18 02:31:55 +0800

    feat: mobile responsiveness

--- components/RegistrationFormField.vue
+++ components/RegistrationFormField.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-	<li class="flex flex-col text-white font-medium min-h-8 justify-center sm:justify-start sm:items-center sm:flex-row gap-4">
+	<li class="flex flex-col text-white text-sm lg:text-base font-medium min-h-8 justify-center sm:justify-start sm:items-center sm:flex-row sm:gap-4">
 		<h2 class="flex items-center justify-start text-gray-300 gap-x-2 min-w-full sm:mr-0 sm:min-w-[20%]">
 			<Icon size="16" :name="icon" />
 			{{ header }}
