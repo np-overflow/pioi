3c884449d5b5ad3e8e6205bb8d6e5e4783faa346
jimmy <s10219505@connect.np.edu.sg>
2022-12-17 15:49:01 +0800

    feat: add select option component

---
+++ components/SelectOption.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<li class="rounded">
+		<div class="py-2 px-4">
+			<slot />
+		</div>
+	</li>
+</template>
+
+<style scoped>
+li:hover {
+    @apply bg-[#141418]
+}
+</style>
