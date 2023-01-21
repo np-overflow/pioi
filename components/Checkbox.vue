a3c3821bd23940fbc6510f00c41e98703b77035c
jimmy <s10219505@connect.np.edu.sg>
2023-01-21 21:59:10 +0800

    feat: add keyboard support to checkbox

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -2,22 +2,27 @@
 const emits = defineEmits(['checked'])
 
 const isChecked = ref(false)
+const check = ref<Nullable<HTMLElement>>(null)
 
 const { enter } = useMagicKeys()
+const { focused } = useFocus(check)
 
 const toggleChecked = () => {
 	isChecked.value = !isChecked.value
 	emits('checked', isChecked.value)
 }
 
-whenever(enter, toggleChecked)
+whenever(enter, () => {
+	if (focused.value) toggleChecked()
+})
 </script>
 
 <template>
-	<Icon
-		size="16"
-		tabindex="0"
-		:name="isChecked ? 'material-symbols:check-box' : 'material-symbols:check-box-outline-blank'"
-		@click="toggleChecked"
-	/>
+	<span ref="check" tabindex="0" @click="toggleChecked" class="focus:outline-0 focus:scale-110 transition-all duration-300 ease-in-out">
+		<Icon
+			ref="check"
+			size="16"
+			:name="isChecked ? 'material-symbols:check-box' : 'material-symbols:check-box-outline-blank'"
+		/>
+	</span>
 </template>
